3e4babaabc98ed4df52876c3235e60dbb159e192
Douglas Rubim <douglasrubims@gmail.com>
2024-10-24 03:14:29 -0300

    chore: trying init

--- Assistant_main.py
+++ Assistant_main.py
@@ -43,7 +43,7 @@ def update_time():
         print("Drawing current time on display")  # Log display update
         display.draw_partial.text((400 - font48.getsize(current_time)[0] // 2, 400), current_time, fill=0, font=font48)
         if debug == 0:
-            epd2.init_part()
+            epd2.init()
             print("Drawing current time on display partial")  # Log display update
             # Xstart = 400 - font48.getsize(current_time)[0] // 2
             # Ystart = 400
@@ -184,7 +184,7 @@ if __name__ == "__main__":
             print("Begin update @" + current_time)
             print("Creating display")
             if debug == 0:
-                epd.init_Fast()
+                epd.init()
             # Update values
             weather.update()
             print("Weather Updated")
