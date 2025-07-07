3ac61dda3d67fe6a503823235ae79240b0ef40ad
Douglas Rubim <douglasrubims@gmail.com>
2025-07-07 00:20:54 -0300

    chore: update news API integration and add requirements file

--- news.py
+++ news.py
@@ -8,18 +8,19 @@ class News:
     pass
 
   def update(self, api_id):
-    print("Getting news from NewsAPI.org")
-    print(f"https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey={api_id}")
+    print("Getting news from NewsData.io")
+    print(f"https://newsdata.io/api/1/latest?apikey={api_id}&country=br&size=5")
     self.news_list = requests.get(
-      f"https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey={api_id}").json()
-    print("News received from NewsAPI.org")
+      f"https://newsdata.io/api/1/latest?apikey={api_id}&country=br&size=5").json()
+    print("News received from NewsData.io")
+    print(self.news_list)
     return self.news_list
 
   def selected_title(self):
     list_news = []
-    if self.news_list["status"] == "ok":
-      for i in range(len(self.news_list["articles"])):
-        line = "> " + self.news_list["articles"][i]["title"]
+    if self.news_list["status"] == "success":
+      for i in range(len(self.news_list["results"])):
+        line = "> " + self.news_list["results"][i]["title"]
         line = textwrap.wrap(line, width=58)
         list_news.append(line)
     else:
